9cab94f006a157ce4c5b6e6b7d4ca120f328c8ad
alfredoperaltag <alfredo08peralta@gmail.com>
2019-03-21 23:39:56 -0600

    datos append

--- public/main.js
+++ public/main.js
@@ -1,1 +1,10 @@
-alert("chat Iniciado");
+$(function () {
+
+    var message = $('#chat-message');
+    var chat = $('#chat');
+
+    $('#message-box').submit(function (e) {
+        e.preventDefault();
+        chat.append(message.val());
+    });
+});
