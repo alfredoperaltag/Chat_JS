e811ef03c3508e72ee23c1c5b820da19e707cfe6
alfredoperaltag <alfredo08peralta@gmail.com>
2019-03-22 09:22:06 -0600

    servidor grafico

--- public/main.js
+++ public/main.js
@@ -3,8 +3,10 @@ $(function () {
     var message = $('#chat-message');
     var chat = $('#chat');
 
+    message.focus();
+
     $('#message-box').submit(function (e) {
         e.preventDefault();
-        chat.append(message.val());
+        chat.append(message.val() + "<br>");
     });
 });
