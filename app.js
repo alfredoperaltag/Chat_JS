e0c1d2adca72d2ebc2015922e92615415ee26419
alfredoperaltag <alfredo08peralta@gmail.com>
2019-03-24 20:46:11 -0600

    estructuracion

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-const io = require('socket.io');
 
 const server = http.createServer(app);
 
@@ -13,12 +12,4 @@ server.listen(app.get('port'), function () {
     console.log("el servidor iniciado")
 })
 
-var sockets = io.listen(server);
-
-sockets.on('connection', function (socket) {
-    console.log("nuevo cliente");
-
-    socket.on('mensaje-del-cliente', function (data) {
-        sockets.emit('mensaje-del-servidor', data);
-    });
-});
+require('./sockets')(server);
