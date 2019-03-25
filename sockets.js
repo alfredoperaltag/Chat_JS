547a12f8aeca4abfa0fdec66872125e42defbf1a
alfredoperaltag <alfredo08peralta@gmail.com>
2019-03-24 20:46:52 -0600

    estructuracion 2.0

---
+++ sockets.js
@@ -0,0 +1,12 @@
+const io = require('socket.io');
+module.exports = function (server) {
+    var sockets = io.listen(server);
+
+    sockets.on('connection', function (socket) {
+        console.log("nuevo cliente");
+
+        socket.on('mensaje-del-cliente', function (data) {
+            sockets.emit('mensaje-del-servidor', data);
+        });
+    });
+}
